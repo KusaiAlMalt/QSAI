993daf3f8a43b665f4752611c5b0a29cc161a211
KusaiAlMalt <215817955+KusaiAlMalt@users.noreply.github.com>
2025-09-14 10:56:17 +0200

    fix: improve spymaster display

--- commands/codenames.js
+++ commands/codenames.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'divides into blue and red team with 1 spymaster each',
     execute(message, args){
         if(args <= 0){
-            message.reply('You provided no names, example of how to use the command:\n!codenames Alice Bob Eve')
+            message.reply('You provided no names, example of how to use the command:\n`!codenames Alice Bob Eve`')
             return
         }
         if(args == 1 && args==='load'){
@@ -18,7 +18,7 @@ module.exports = {
         const redSM = chooseSpyMaster(redTeam);
 
         //embed this message instead of literal
-        message.reply(`Blue Team: ${blueTeam.join(', ')} (SpyMaster: ${blueSM})\nRed Team: ${redTeam.join(', ')} (SpyMaster: ${redSM})`);
+        message.reply(`Blue Team: ${blueTeam.join(', ')} (**Spymaster: ${blueSM}**)\nRed Team: ${redTeam.join(', ')} (**Spymaster: ${redSM})**`);
     }
 }
 
