048ac5feea77c1c5fa967d0f00a9f4bd1dddc35f
KusaiAlMalt <215817955+KusaiAlMalt@users.noreply.github.com>
2025-09-14 10:22:35 +0200

    refactor: work with args rather than listing to next message

--- commands/codenames.js
+++ commands/codenames.js
@@ -1,27 +1,29 @@
-const { divideTeams } = require('../utils/teamDivider.js');
+const { divideIntoTwoTeams } = require('../utils/teamDivider.js');
 
 module.exports = {
     name: 'codenames',
     description: 'divides into blue and red team with 1 spymaster each',
-    async execute(message, args){
-        await message.reply(`Provide players' names seperated by a comma (,)`);
-
-        const filter = m => m.author.id === message.author.id;
-        try{
-        const collected = await message.channel.awaitMessages({
-            filter,
-            max:1,
-            time:15000,
-            errors:['time']
-        });
-
-        const reply = collected.first();
-        const nameList = reply.content.split(',');
-        const teams = divideTeams(nameList);
-        message.channel.send(`Blue Team: ${teams[0].join(', ')}\nRed Team: ${teams[1].join(', ')}`);
-
-        }catch (e){
-        message.channel.send(`You took too long, please try again. Error ${e}`);
+    execute(message, args){
+        if(args <= 0){
+            message.reply('You provided no names, example of how to use the command:\n!codenames Alice Bob Eve')
+            return
         }
+        if(args == 1 && args==='load'){
+            //load last used names and randomize
+        }
+
+        const [blueTeam, redTeam] = divideIntoTwoTeams(args);
+
+        const blueSM = chooseSpyMaster(teamNames);
+        const redSM = chooseSpyMaster(teamNames);
+
+
+
+        //embed this message instead of literal
+        message.reply(`Blue Team: ${blueTeam.join(', ')}\nRed Team: ${redTeam.join(', ')}`);
     }
+}
+
+function chooseSpyMaster(teamNames){
+
 }
