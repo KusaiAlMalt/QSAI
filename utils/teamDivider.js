791ea8b6a9df86f32d127924bc173b807caab3d0
KusaiAlMalt <215817955+KusaiAlMalt@users.noreply.github.com>
2025-09-14 10:47:21 +0200

    chore: follow exports best practices

--- utils/teamDivider.js
+++ utils/teamDivider.js
@@ -1,30 +1,30 @@
-module.exports = {
-    divideIntoTwoTeams(namesList){
+function divideIntoTwoTeams(namesList){
 
-        var len = namesList.length;
-        var firstTeam = [];
-        var secondTeam = [];
-        
-        var indexSet = new Set();
+    var len = namesList.length;
+    var firstTeam = [];
+    var secondTeam = [];
+    
+    var indexSet = new Set();
 
-        var randInt = Math.floor(Math.random()*namesList.length);
+    var randInt = Math.floor(Math.random()*namesList.length);
 
-        while (len>0){
-            while(indexSet.has(randInt)){
-            randInt = Math.floor(Math.random()*namesList.length);
-            }
-
-            if (len%2===0){
-                firstTeam.push(namesList[randInt]);
-            }
-            else{
-                secondTeam.push(namesList[randInt]);
-            }
-            
-            indexSet.add(randInt)
-            len--;
+    while (len>0){
+        while(indexSet.has(randInt)){
+        randInt = Math.floor(Math.random()*namesList.length);
         }
 
-        return [firstTeam, secondTeam];
+        if (len%2===0){
+            firstTeam.push(namesList[randInt]);
+        }
+        else{
+            secondTeam.push(namesList[randInt]);
+        }
+        
+        indexSet.add(randInt)
+        len--;
     }
+
+    return [firstTeam, secondTeam];
 }
+
+module.exports = {divideIntoTwoTeams}
