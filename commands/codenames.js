dae565643bef8a438b54606b9d72b51cb70ae0ed
KusaiAlMalt <215817955+KusaiAlMalt@users.noreply.github.com>
2025-09-14 11:19:07 +0200

    fix: improve command response and spymaster selection logic in codenames

--- commands/codenames.js
+++ commands/codenames.js
@@ -3,25 +3,37 @@ const { divideIntoTwoTeams } = require('../utils/teamDivider.js');
 module.exports = {
     name: 'codenames',
     description: 'divides into blue and red team with 1 spymaster each',
+    example: 'Example of how to use the command:\n`!codenames Alice Bob Carol Eve`',
     execute(message, args){
-        if(args <= 0){
-            message.reply('You provided no names, example of how to use the command:\n`!codenames Alice Bob Eve`')
+        if(args.length <= 0){
+            message.reply(`You provided no names. ${this.example}`)
             return
         }
-        if(args == 1 && args==='load'){
+        if(args.length === 1 && args==='load'){
             //load last used names
+        }
+        if(args.length < 4){
+            message.reply(`Provide at least four names. ${this.example}`)
+            return
         }
 
         const [blueTeam, redTeam] = divideIntoTwoTeams(args);
 
-        const blueSM = chooseSpyMaster(blueTeam);
-        const redSM = chooseSpyMaster(redTeam);
+        const [[blueSpymaster], blueOperatives] = pickSpymasterAndOperative(blueTeam);
+        const [[redSpymaster], redOperatives] = pickSpymasterAndOperative(redTeam);
+
+        const blueMessage = `__Blue Team__:\nOperatives: ${blueOperatives.join(' ')}\nSpymaster: ${blueSpymaster}`
+        const redMessage = `__Red Team__:\nOperatives: ${redOperatives.join(' ')}\nSpymaster: ${redSpymaster}`
 
         //embed this message instead of literal
-        message.reply(`Blue Team: ${blueTeam.join(', ')} (**Spymaster: ${blueSM}**)\nRed Team: ${redTeam.join(', ')} (**Spymaster: ${redSM})**`);
+        message.reply(`${redMessage}\n${blueMessage}`);
     }
 }
 
-function chooseSpyMaster(teamNames){
-    return teamNames[Math.floor(Math.random()*teamNames.length)];
+function pickSpymasterAndOperative(teamNames){
+    const spyIndex = Math.floor(Math.random()*teamNames.length);
+    const spymaster = teamNames[spyIndex];
+    const operatives = teamNames.filter((_, i) => i !== spyIndex);
+    
+    return [[spymaster], operatives];
 }
