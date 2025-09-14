a5a23222c3c090df3ae27fa5319ac13c7f2caf27
KusaiAlMalt <215817955+KusaiAlMalt@users.noreply.github.com>
2025-09-14 09:48:06 +0200

    refactor:  module/function commands (ping only)

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 require('dotenv').config();
 
 const { Client, GatewayIntentBits, Collector } = require('discord.js');
-const { divideTeams } = require('./TeamDivider.js');
+const { divideTeams } = require('./teamDivider.js');
+
+//Could be refactored in its own setup step and return the map of the commands with their names, sepereation of concerns
+const fs = require('fs');
+const path = require('path');
+
+const commands = new Map();
+
+const commandFiles = fs.readdirSync(path.join(__dirname, 'commands'))
+                        .filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles){
+  const command = require(`./commands/${file}`);
+  commands.set(command.name, command);
+}
 
 const client = new Client({
   intents: [
@@ -13,19 +27,38 @@ const client = new Client({
 
 client.login(process.env.DISCORD_TOKEN);
 
-client.once('ready', () => {
+client.once('clientReady', () => {
   console.log(`✅ Logged in as ${client.user.tag}`);
 });
 
-client.on('messageCreate', message => {
-  if (message.content === '!ping') {
-    message.reply('Pong!');
+//TODO test command
+const PREFIX = '!'
+client.on('messageCreate', message =>{
+  if(!message.content.startsWith(PREFIX) || message.author.bot) return;
+
+  const args = message.content.slice(PREFIX.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+  const command = commands.get(commandName);
+  if(!command) return;
+  
+  try{
+    command.execute(message, args);
+  }catch (error) {
+    console.error(error);
+    message.reply('Error Executing Command');
   }
 });
 
+/*client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.reply('Pong!');
+  }
+});*/
+
 client.on('messageCreate', async message => {
   if (message.content === '!codenames'){
-    await message.reply('Provide player names seperated by a comma (,)');
+    await message.reply(`Provide players' names seperated by a comma (,)`);
 
     const filter = m => m.author.id === message.author.id;
     try{
@@ -42,7 +75,7 @@ client.on('messageCreate', async message => {
       message.channel.send(`Blue Team: ${teams[0].join(', ')}\nRed Team: ${teams[1].join(', ')}`);
 
     }catch{
-      message.channel.send('You ass too slow');
+      message.channel.send('You took too long, please try again');
     }
   }
 });
